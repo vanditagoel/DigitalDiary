4acec350e7b038e2e0b084df0924d15790ba865d
Vandita Goel <goelvandita777@gmail.com>
2025-05-31 19:12:40 +0530

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 import bcrypt from "bcryptjs";
-import emailjs from 'emailjs-com';
 
 function Home() {
   const navigate = useNavigate();
